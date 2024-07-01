252665bf7d6c42fb8ec92198d81c7ee61bb5aad5
erhant <erhany96@gmail.com>
2024-07-01 21:00:59 +0300

    todo syntax design

--- README.md
+++ README.md
@@ -1,11 +1,55 @@
 # Polenta
 
-Polenta is a terminal application to play around with polynomials. 
+Polenta is a polynomial arithmetic language.
 
-## Syntax
+## Creating a Polynomial
 
-Polenta comes with a language, with the following syntax:
+A polynomial is created by specifying its terms, which is in order: (coefficient, term, exponent). It is assigned to an identifier, in the form `let <uppercase_id> = <polynomial>`.
 
-```lua
+The expression below stands for $p = 3x^2 + 5$.
 
+```rs
+let P = 3*x^2 + 5;
+```
+
+Behind the scenes all terms are considered a monomial, and they are added together. As such, you can repeat a term with the same degree and the result will have them added up:
+
+```rs
+let P = 3*x^2 + 2*x^2 + 1*x^2; // equals 6*x^2
+let Q = 5*x - 5*x + 1; // equals 1
+```
+
+You can also multiply polynomials like:
+
+```rs
+let P = (x - 1)*(x - 2)*(x - 4);
+```
+
+You can create a polynomial from an existing one:
+
+```rs
+let P = 3*x;
+let Q = P^2 + 2*P; // (3*x)^2 + 2*(3*x) = 9*x^2 + 6*x
+```
+
+Polenta allows shadowing to overwrite existing polynomials:
+
+```rs
+let P = 3*x;
+let P = 3*P + 5; // 9*x + 5
+```
+
+## Evaluating A Polynomial
+
+The notation of polynomial evaluation is done by simply "calling" the polynomial's identifier.
+
+```rs
+let P = 3*x;
+let P_3 = P(3); // 9
+```
+
+We can later use these values if we would like:
+
+```rs
+let Q = x^P_3 - P_3*x; // x^9 - 9*x
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
-use lambdaworks_math::{
-    field::{element::FieldElement, fields::u64_goldilocks_field::Goldilocks64Field},
-    polynomial::Polynomial,
-};
-use pest::Parser;
-use polenta::grammar::{parse, parse_term, PolentaRule, TermSign};
+use lambdaworks_math::field::fields::u64_goldilocks_field::Goldilocks64Field;
+use polenta::grammar::{parse, Polenta};
 
 type F = Goldilocks64Field;
 
 fn main() {
     const INPUT: &str = "2*x - 2*x + 5";
-    let mut program = parse(INPUT).expect("should parse");
 
-    let mut last_sign = TermSign::default();
-    let mut poly = Polynomial::<FieldElement<F>>::zero();
-    for poly_pair in program.next().unwrap().into_inner() {
-        match poly_pair.as_rule() {
-            PolentaRule::Term => {
-                let monomial = parse_term::<F>(poly_pair);
-                poly = poly
-                    + if last_sign == TermSign::Positive {
-                        monomial
-                    } else {
-                        -monomial
-                    };
-            }
-            PolentaRule::OpAdd => {
-                last_sign = TermSign::from(poly_pair.as_str());
-            }
-            _ => {
-                println!("{:?}", poly_pair);
-                unreachable!()
-            }
-        }
-    }
+    let mut polenta = Polenta::<F>::new();
+    let poly = polenta.parse_polynomial(parse(INPUT).unwrap().next().unwrap());
 
     println!("Parsed polynomial: {:?}", poly);
 }
